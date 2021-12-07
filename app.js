0474752b62073c2d8887c1e0bb4a707b2395eac7
Salman Farshe <salmanfarshe39@gmail.com>
2021-12-07 21:22:37 +0600

    Add Notes

---
+++ app.js
@@ -0,0 +1,33 @@
+// import modules/packages
+const yargs = require('yargs')
+const chalk = require('chalk-v2')
+const fs = require('fs-extra')
+const { argv } = require('process')
+// custom modules
+const notes = require('./notes')
+
+// ======================== Add Command ===================
+// pass an object
+yargs.command({
+    command: 'add',
+    describe: 'Add a note',
+    builder: {
+        title: {
+            describe: 'Title of a Notes',
+            demandOption: true,     // required
+            type: 'string'          // input type
+        },
+        body: {
+            describe: 'Body of a Notes',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        // this function going to run, when someone uses that (add) command.
+        // argv = contains all of the argumants provided into the builder
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+console.log(yargs.argv)
---
+++ notes.js
@@ -0,0 +1,50 @@
+const chalk = require('chalk-v2')
+const fs = require('fs-extra')
+
+// ============================ Re-Usable code ===========================
+// save notes
+const saveNote = (note) => {
+    const dataJSON = JSON.stringify(note)   // convert Object to JSON
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+// load notes
+const loadNote = () => {
+    try{
+        const dataBuffer = fs.readFileSync('notes.json')    // binary data
+        const dataJSON = dataBuffer.toString()         // binary to String data
+        return JSON.parse(dataJSON)                   // JSON to Object
+    } catch(e){
+        return []       // empty array list
+    }
+}
+
+
+// ========================= add notes =====================
+const addNote = (title, body) => {
+    const note = loadNote()
+    // check duplication of input data
+    const duplicate = note.filter(notes => {
+        return notes.title === title
+    })
+
+    if(duplicate.length == 0){
+        // push into array
+        note.push({
+            title: title,
+            body: body
+        })
+        // save newly created data
+        saveNote(note)
+        console.log(chalk.green('Notes Added Successfully..'))
+    } else{
+        console.log(chalk.red('Notes Already Taken.. Please Try Another'))
+    }
+}
+
+
+
+// exports module into the app.js file
+module.exports = {
+    addNote: addNote
+}
